089944e26c710f5fbd8817df797082697481ac2d
MarciX1 <110532239+MarciX1@users.noreply.github.com>
2023-09-20 18:26:35 +0200

    Added animation, more features

--- script.js
+++ script.js
@@ -1,46 +1,70 @@
 const resetBtn = document.querySelector(".reset-btn");
 const rollBtn = document.querySelector(".roll-btn");
 const dice = document.querySelector(".dice");
+const diceNum = document.querySelector(".diceNum");
 
 resetBtn.addEventListener("click", () => {
     dice.innerHTML = "";
+    diceNum.textContent = "";
 });
 
 rollBtn.addEventListener("click", () => {
-    dice.innerHTML = "";
-    randomNumLogic();
+    rollBtn.disabled = true;
+    diceInterval();
 });
 
 function randomNumLogic() {
+    dice.innerHTML = "";
     const randomNum = Math.floor(Math.random() * 6) + 1;
 
     if (randomNum === 1) {
         const diceCircle = `<div class="circle"></div>`;
         dice.innerHTML = diceCircle;
+        diceNum.textContent = "1";
     } else if (randomNum === 2) {
         for (num = 0; num < randomNum; num++) {
             const diceCircle = `<div class="circle circle-2"></div>`;
             dice.innerHTML += diceCircle;
         }
+        diceNum.textContent = "2";
     } else if (randomNum === 3) {
         for (num = 0; num < randomNum; num++) {
             const diceCircle = `<div class="circle circle-3"></div>`;
             dice.innerHTML += diceCircle;
         }
+        diceNum.textContent = "3";
     } else if (randomNum === 4) {
         for (num = 0; num < randomNum; num++) {
             const diceCircle = `<div class="circle circle-4"></div>`;
             dice.innerHTML += diceCircle;
         }
+        diceNum.textContent = "4";
     } else if (randomNum === 5) {
         for (num = 0; num < randomNum; num++) {
             const diceCircle = `<div class="circle circle-5"></div>`;
             dice.innerHTML += diceCircle;
         }
+        diceNum.textContent = "5";
     } else if (randomNum === 6) {
         for (num = 0; num < randomNum; num++) {
             const diceCircle = `<div class="circle circle-6"></div>`;
             dice.innerHTML += diceCircle;
         }
+        diceNum.textContent = "6";
     }
+
+}
+
+function diceInterval() {
+    let counter = 0;
+    const diceRandom = setInterval(() => {
+        randomNumLogic();
+        counter += 1;
+
+        if (counter >= 20) {
+            clearInterval(diceRandom);
+            rollBtn.disabled = false;
+        }
+    }, 100);
+    
 }
